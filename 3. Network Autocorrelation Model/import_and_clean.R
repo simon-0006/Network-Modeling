878fb071fa4293d96d1e1feb7047759f4c9e2331
Josia John <josia.j.john@gmail.com>
2026-03-17 10:35:23 +0100

    Task 3: use proper lnam model, not lm!

--- 3. Network Autocorrelation Model/import_and_clean.R
+++ 3. Network Autocorrelation Model/import_and_clean.R
@@ -1,4 +1,6 @@
 #------------------------------- IMPORTING -------------------------------------
+set.seed(161)
+
 
 if (!require(sna)) install.packages("sna")
 if (!require(here)) install.packages("here")
@@ -17,7 +19,7 @@ MatricesRaw <- lapply(dataList, as.matrix)
 
 getNet <- function(classroom) {
     w2net <- MatricesRaw[[paste(classroom, "_W2", sep="")]]
-    colnames(w2net) <- sub("^X0*", "", colnames(w2net))
+    colnames(w2net) <- rownames((w2net))
     ok_ids1 <- rownames(w2net)[complete.cases(w2net)]
 
 
@@ -27,12 +29,8 @@ getNet <- function(classroom) {
     ok_ids <- intersect(ok_ids1, ok_ids2)
 
     w2netClean <- as.matrix(w2net[ok_ids,ok_ids])
-    attrClean <- as.data.frame(attr[ok_ids, ])
-    attrClean$HISEI <- as.numeric(attrClean$HISEI)
-    attrClean$literacy_end <- as.numeric(attrClean$literacy_end)
+    w2netClean <- w2netClean / pmax(rowSums(w2netClean),1)
 
-    w2netClean <- w2netClean / rowSums(w2netClean)
-    print(length(ok_ids))
     return(w2netClean)
 }
 
@@ -41,13 +39,11 @@ getAttr <- function(classroom) {
     colnames(w2net) <- sub("^X0*", "", colnames(w2net))
     ok_ids1 <- rownames(w2net)[complete.cases(w2net)]
 
-
     attr <- MatricesRaw[["attr"]]
     ok_ids2 <- rownames(attr)[complete.cases(attr)]
 
     ok_ids <- intersect(ok_ids1, ok_ids2)
 
-    w2netClean <- as.matrix(w2net[ok_ids,ok_ids])
     attrClean <- as.data.frame(attr[ok_ids, ])
     attrClean$HISEI <- as.numeric(attrClean$HISEI)
     attrClean$literacy_end <- as.numeric(attrClean$literacy_end)
